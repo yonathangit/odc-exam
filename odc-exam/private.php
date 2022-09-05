e6d5f1ae57300f801882fd3a33b549bdf0152af8
yonathangit <101411001+yonathangit@users.noreply.github.com>
2022-09-05 16:43:51 +0300

    Add files via upload

---
+++ odc-exam/private.php
@@ -0,0 +1,28 @@
+<?php
+
+//echo "Cookies: " . $_COOKIE['auth_id'];
+
+
+include "database.php";
+include "restricted.php";
+
+
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>Private</title>
+</head>
+
+<body>
+
+<h3>Private area!</h3>
+
+<a href="logoff.php">Logoff</a>
+
+</body>
+
+</html>
