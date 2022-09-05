3c5498ac0be84a01f02f241cb9c554244d397955
yonathangit <101411001+yonathangit@users.noreply.github.com>
2022-09-05 16:50:45 +0300

    Add files via upload

---
+++ odc-exam/logoff.php
@@ -0,0 +1,18 @@
+<?php
+    setcookie("auth_id","");
+    setcookie("auth_email","");
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>Logoff</title>
+</head>
+
+<body>
+    <h1>Logoff successful!</h1>
+
+</body>
+
+</html>
